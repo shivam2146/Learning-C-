f4a48855d539fb391a8773273741df7a33e0f485
Shivam Agarwal <24.shivamagarwal@gmail.com>
2017-11-12 00:38:33 +0530

    templates and classes

---
+++ templates_class.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+template<class T,class U=char>
+class test{
+	T mem1;
+	U mem2;
+  public:
+	test(T a,U b);	
+	void display();
+};
+
+template<class T,class U>
+test<T,U>::test(T a,U b){
+	mem1 = a;
+	mem2 = b;	
+	
+}
+
+template<class T,class U>
+void test<T,U>::display(){
+	cout << "members are: " << mem1 << " " << mem2;
+}
+
+
+int main(){
+	test<int> obj(1,'a');
+	obj.display();
+	test<int,float> obj1(1,'a');  //takes a ascii value as float i.e 97
+	obj1.display();
+
+}

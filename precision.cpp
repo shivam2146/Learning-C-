ac9d059a4a15625cb181815ff413e3e1c88be29e
Shivam Agarwal <shivam.mcs17.du@gmail.com>
2017-12-24 02:20:48 +0530

    Update precision.cpp

--- precision.cpp
+++ precision.cpp
@@ -6,6 +6,7 @@ double a = 4.223234232;
 double b = 2323.0;
 
 cout.precision(6);
+  //cout<<setprecision(9);          //works the same way diff is setprecision is iomanipulater and prcision() is mem function
 
 cout<<"Normal values of floating point numbers\n a=";
 cout<<a<<"\nb=\n"<<b;
@@ -15,7 +16,7 @@ cout<<a<<"\n"<<b;
 
 cout<<"\n\n\nvalues using scientific\n"<<std::scientific;
 cout<<a<<"\n"<<b;
-
+//cout<<cout.precision();    returns value of current precision
 cout<<"\n\n\nvalues using hexfloat\n"<<std::hexfloat;
 cout<<a<<"\n"<<b;
 

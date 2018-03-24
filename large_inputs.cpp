fff1051b6821a48cc8e7f27d152b0c1f418a0130
shivam2146 <shivam.mcs17.du@gmail.com>
2018-03-25 00:07:42 +0530

    handle large inputs

---
+++ large_inputs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long l= 1000000000000000000;  //can store 10^18 but overflow with 10^19
+  unsigned long long l1 = 10000000000000000000; //can store upto 10^19
+  cout<<l<<endl;
+  cout<<l1<<endl;
+  cout<<"largest value long long int hold is: "<<LLONG_MAX<<endl; //9223372036854775807
+  cout<<"largest value unsigned long long hold is: "<<ULLONG_MAX<<endl;   //18446744073709551615
+
+  int n= 1000000000; //can store 10^9
+  cout<<n<<endl;
+  unsigned int n1= 1000000000; //same 10^9
+  cout<<n1<<endl;
+  cout<<"largest value int hold is: "<<INT_MAX<<endl; //2147483647
+  cout<<"largest value unsigned int hold is: "<<UINT_MAX<<endl;   //4294967295
+
+__int128_t lar= 10000000000000000000;
+// /cout<<"largest value we can hold: "<<__cpp_raw_strings(lar);
+
+
+
+}

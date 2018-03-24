98e31439fb41dd485bd56bbc4407232b251e05bb
shivam2146 <shivam.mcs17.du@gmail.com>
2018-03-25 00:13:13 +0530

    using macros

---
+++ macros.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sq(a) (a)*(a)
+#define rep(i,a,b) for(i=a;i<b;i++)
+int main() {
+  cout<<sq(3+3)<<"\n";
+  int a[]={1,2,3,4,5};
+  int i;
+  rep(i,0,5)
+    cout<<a[i];
+  return 0;
+}
